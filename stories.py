a5f3c5077718b093d9200f67e0e405c81302efa3
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2023-07-13 10:54:08 +0100

    Add stories.py with story template

---
+++ stories.py
@@ -0,0 +1,9 @@
+def get_story():
+    story = """
+    The [ADJECTIVE] [NOUN] [VERB] [ADVERB] while [VERB] the [NOUN].
+    Suddenly, a [ADJECTIVE] [NOUN] [VERB] [ADVERB] into the scene, causing [NOUN] to [VERB] in [ADJECTIVE] surprise.
+    Everyone [VERB] and [VERB] as the [NOUN] [VERB] [ADVERB] away.
+    The crowd [VERB] in [ADJECTIVE] anticipation, wondering what would happen next.
+    """
+    return story
+
