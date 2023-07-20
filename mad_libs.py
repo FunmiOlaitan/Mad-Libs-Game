b6b1f1fab73d6b23bb620753e559f0fd6429148f
Funmilola Olaitan <funmilolaolaitan@yahoo.com>
2023-07-20 10:47:07 +0100

    fix: Fix string literal bug

--- mad_libs.py
+++ mad_libs.py
@@ -1,18 +1,18 @@
-# imports the 'get_story' function from the story module
+# Imports the 'get_story' function from the story module
 from stories import get_story
 
-# promt the user for word inputs 
+# Promt the user for word inputs 
 def prompt_word(word_type):
-    return(f'Enter a {word_type}')
+    return input(f"Enter a {word_type}: ")
 
-# generate the story based on those inputs 
+# Generate the story based on those inputs
 def generate_story(adjective, noun1, verb1, adverb1, verb2, noun2, adjective2, verb3, adverb2, verb4, adjective3):
     story_template = get_story()
-    story = story_template.replace("[ADJECTIVE]" ,adjective).replace("[NOUN]", noun1).replace("[VERB]", verb1).replce("[ADVERB]", adverb1).replace("[VERB]", verb2).replace("[NOUN]", noun2).replace("[ADJECTIVE]", adjective2).replace("[VERB]", verb3).replace("[ADVERB]", adverb2).replace("[VERB]", verb4).replace("[ADJECTIVE]", adjective3)
+    story = story_template.replace("[ADJECTIVE]" ,adjective).replace("[NOUN]", noun1).replace("[VERB]", verb1).replace("[ADVERB]", adverb1).replace("[VERB]", verb2).replace("[NOUN]", noun2).replace("[ADJECTIVE]", adjective2).replace("[VERB]", verb3).replace("[ADVERB]", adverb2).replace("[VERB]", verb4).replace("[ADJECTIVE]", adjective3)
     return story
 
 # print the final story
-def play_mad_libs_game()
+def play_mad_libs_game():
     adjective = prompt_word("adjective")
     noun1 = prompt_word("noun")
     verb1 = prompt_word("verb")
@@ -24,6 +24,7 @@ def play_mad_libs_game()
     adverb2 = prompt_word("adverb")
     verb4 = prompt_word("verb")
     adjective3 = prompt_word("adjective")
+
 
     story = generate_story(adjective, noun1, verb1, adverb1, verb2, noun2, adjective2, verb3, adverb2, verb4, adjective3)
     print("\nYour Mad Libs Story:")
